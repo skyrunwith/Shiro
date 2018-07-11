232bf2ca4aab5ac5b5f8df56a2bb41e29576c03d
skyrunwith <582547045@qq.com>
2018-07-11 21:54:31 +0800

    shiro

---
+++ src/main/java/com/fzd/shiro/chapter02/realm/MyRealm2.java
@@ -0,0 +1,36 @@
+package com.fzd.shiro.chapter02.realm;
+
+import org.apache.shiro.authc.*;
+import org.apache.shiro.realm.Realm;
+
+/**
+ * Created by FZD on 2018/4/19.
+ * Description:
+ */
+public class MyRealm2 implements Realm {
+
+    @Override
+    public String getName() {
+        return "myrealm2";
+    }
+
+    @Override
+    public boolean supports(AuthenticationToken token) {
+        return token instanceof UsernamePasswordToken; //仅支持UsernamePasswordToken类型的Token
+    }
+
+    @Override
+    public AuthenticationInfo getAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+
+        String username = (String)token.getPrincipal();  //得到用户名
+        String password = new String((char[])token.getCredentials()); //得到密码
+        if(!"wang".equals(username)) {
+            throw new UnknownAccountException(); //如果用户名错误
+        }
+        if(!"123".equals(password)) {
+            throw new IncorrectCredentialsException(); //如果密码错误
+        }
+        //如果身份认证验证成功，返回一个AuthenticationInfo实现；
+        return new SimpleAuthenticationInfo(username, password, getName());
+    }
+}
---
+++ src/main/java/com/fzd/shiro/chapter02/realm/MyRealm3.java
@@ -0,0 +1,34 @@
+package com.fzd.shiro.chapter02.realm;
+
+import org.apache.shiro.authc.*;
+import org.apache.shiro.realm.Realm;
+
+/**
+ * Created by FZD on 2018/4/19.
+ * Description:
+ */
+public class MyRealm3 implements Realm{
+    @Override
+    public String getName() {
+        return "myrealm3";
+    }
+    @Override
+    public boolean supports(AuthenticationToken token) {
+        //仅支持UsernamePasswordToken类型的Token
+        return token instanceof UsernamePasswordToken;
+    }
+    @Override
+    public AuthenticationInfo getAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+
+        String username = (String)token.getPrincipal();  //得到用户名
+        String password = new String((char[])token.getCredentials()); //得到密码
+        if(!"zhang".equals(username)) {
+            throw new UnknownAccountException(); //如果用户名错误
+        }
+        if(!"123".equals(password)) {
+            throw new IncorrectCredentialsException(); //如果密码错误
+        }
+        //如果身份认证验证成功，返回一个AuthenticationInfo实现；
+        return new SimpleAuthenticationInfo(username + "@163.com", password, getName());
+    }
+}
